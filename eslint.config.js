5fa285c53e3da0484417bd9c5d8771cf390e1c05
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-02-24 20:18:04 +0330

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**", "build/**"]
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: { ...globals.node }
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"]
+    }
+  }
+];
